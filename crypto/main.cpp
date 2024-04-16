0696766b46acd693f73155bbe92d2782224e2818
Andrea Quagliotti <104375703+and05990@users.noreply.github.com>
2024-04-16 21:30:46 +0200

    Update main.cpp

--- crypto/main.cpp
+++ crypto/main.cpp
@@ -10,36 +10,24 @@ int main()
 {
     srand(time(NULL)); // Seed the random number generator with the current time
 
-    #include <iostream>
-    #include <cstdlib>
-    #include <ctime>
-    #include <chrono>
-    #include <thread>
+    double bitcoinValue = 10000.0; // Starting value of Bitcoin
+    double volatility = 0.01; // Volatility factor (percentage change per update)
 
-    using namespace std;
-
-    int main()
+    while (true)
     {
-        srand(time(NULL)); // Seed the random number generator with the current time
+        // Generate a random percentage change in the Bitcoin value
+        double percentageChange = (rand() % 200 - 100) / 100.0 * volatility;
 
-        double bitcoinValue = 10000.0; // Starting value of Bitcoin
-        double volatility = 0.01; // Volatility factor (percentage change per update)
+        // Update the Bitcoin value
+        bitcoinValue *= (1.0 + percentageChange);
 
-        while (true)
-        {
-            // Generate a random percentage change in the Bitcoin value
-            double percentageChange = (rand() % 200 - 100) / 100.0 * volatility;
+        // Print the updated Bitcoin value
+        cout << "Bitcoin value: $" << bitcoinValue << endl;
 
-            // Update the Bitcoin value
-            bitcoinValue *= (1.0 + percentageChange);
-
-            // Print the updated Bitcoin value
-            cout << "Bitcoin value: $" << bitcoinValue << endl;
-
-            // Wait for 3 seconds before updating again
-            using namespace std::chrono;
-            this_thread::sleep_for(seconds(3));
-        }
-
-        return 0;
+        // Wait for 3 seconds before updating again
+        using namespace std::chrono;
+        this_thread::sleep_for(seconds(3));
     }
+
+    return 0;
+}
